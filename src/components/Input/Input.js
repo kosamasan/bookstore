cf4ddd8988a7b0865123085a6e47cb40fa88555a
Unknown <kosmasa@ceid.upatras.gr>
2019-12-20 21:30:06 +0200

    Create book and fields validation

---
+++ src/components/Input/Input.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+import  './Input.css';
+
+const input = ( props ) => {
+    let inputElement = null;
+    let validationError = null;
+    const inputClasses = ['form-control'];
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push('invalid');
+        validationError =  <div className='alert alert-danger'>{props.errorMessage}</div>;;
+    }
+
+    switch ( props.elementType ) {
+        case ( 'input' ):
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ( 'textarea' ):
+            inputElement = <textarea
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+            break;
+        case ( 'select' ):
+            inputElement = (
+                <select
+                    className={inputClasses.join(' ')}
+                    value={props.value}
+                    onChange={props.changed}>
+                    {props.elementConfig.options.map(option => (
+                        <option key={option.value} value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}
+                </select>
+            );
+            break;
+        default:
+            inputElement = <input
+                className={inputClasses.join(' ')}
+                {...props.elementConfig}
+                value={props.value}
+                onChange={props.changed} />;
+    }
+
+    return (
+        <div className='input'>
+            {/* <label className='control-label'>{props.label}</label> */}
+            {inputElement}
+            {validationError}
+        </div>
+    );
+
+};
+
+export default input;
--- src/containers/CreateBook.js
+++ src/containers/CreateBook.js
@@ -1,11 +1,288 @@
 import React, { Component } from 'react';
 
+import Spinner from '../components/Spinner/Spinner';
+import Input from '../components/Input/Input';
+
 class CreateBook extends Component {
-  render () {
-    return (
-      <div>Create Book</div>
-    );
-  }
+    state = {
+        orderForm: {
+            title: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Title'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 10,
+                    maxLength: 120,
+                },
+                errorMessage: 'Invalid Title. Title should be between 10 and 120 characters.',
+                valid: false,
+                touched: false
+            },
+            description: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Description'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    maxLength: 512,
+                    starstsWithCapital: true
+                },
+                errorMessage: 'Invalid description. Description should shorter than 512 characters and start with capital letter.',
+                valid: false,
+                touched: false
+            },
+            publisher: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Publisher'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 5,
+                    maxLength: 60,
+                },
+                errorMessage: 'Invalid publisher. Publisher should be between 5 and 60 characters.',
+                valid: false,
+                touched: false
+            },
+            year: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Year'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 4,
+                    maxLength: 4,
+                    isNumeric: true
+                },
+                errorMessage: 'Invalid year. Year should be numeric with 4 characters.',
+                valid: false,
+                touched: false
+            },
+            mumberOfPages: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Number of Pages'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    maxLength: 4,
+                    isNumeric: true,
+                    max: 9999
+                },
+                errorMessage: 'Invalid Number of Pages. It should be up to 9999 pages.',
+                valid: false,
+                touched: false
+            },
+            isbn10: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ISBN-10'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 10,
+                    maxLength: 10,
+                    isNumeric: true
+                },
+                errorMessage: 'Invalid Number of Pages. It should be 10 characters.',
+                valid: false,
+                touched: false
+            },
+            isbn13: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ISBN-13'
+                },
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 13,
+                    maxLength: 13,
+                    isNumeric: true
+                },
+                errorMessage: 'Invalid Number of Pages. It should be 13 characters.',
+                valid: false,
+                touched: false
+            },
+            category: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Category (Add up to four comma seperated)'
+                },
+                value: '',
+                validation: {
+                    upToFourStrings: true,
+                    maxLength: 60,
+                },
+                errorMessage: 'Invalid category. It should be up to four comma seperated.',
+                valid: false,
+                touched: false
+            },
+            author: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Author(s) (Add up to three comma seperated)'
+                },
+                value: '',
+                validation: {
+                    upToThreeStrings: true,
+                    maxLength: 60,
+                },
+                errorMessage: 'Invalid author. It should be up to three comma seperated.',
+                valid: false,
+                touched: false
+            }
+        },
+        formIsValid: false,
+        loading: false
+    }
+
+    orderHandler = ( event ) => {
+        event.preventDefault();
+        this.setState( { loading: true } );
+        const formData = {};
+        for (let formElementIdentifier in this.state.orderForm) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value;
+        }
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            orderData: formData
+        }
+        // axios.post( '/orders.json', order )
+        //     .then( response => {
+        //         this.setState( { loading: false } );
+        //         this.props.history.push( '/' );
+        //     } )
+        //     .catch( error => {
+        //         this.setState( { loading: false } );
+        //     } );
+    }
+
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (!rules) {
+            return true;
+        }
+        
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.starstsWithCapital) {
+            const pattern = /^[A-Z]/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.upToThreeStrings) {
+            const pattern = /^[^,]+(?:,[^,]+){0,2}$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.upToFourStrings) {
+            const pattern = /^[^,]+(?:,[^,]+){0,3}$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler = (event, inputIdentifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        };
+        const updatedFormElement = { 
+            ...updatedOrderForm[inputIdentifier]
+        };
+        updatedFormElement.value = event.target.value;
+        updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+        updatedFormElement.touched = true;
+        updatedOrderForm[inputIdentifier] = updatedFormElement;
+        
+        let formIsValid = true;
+        for (let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
+    }
+
+    render () {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+        let form = (
+            <form className='form-horizontal bookForm' onSubmit={this.orderHandler}>
+                {formElementsArray.map(formElement => (
+                    <Input 
+                        key={formElement.id}
+                        elementType={formElement.config.elementType}
+                        elementConfig={formElement.config.elementConfig}
+                        value={formElement.config.value}
+                        invalid={!formElement.config.valid}
+                        shouldValidate={formElement.config.validation}
+                        errorMessage={formElement.config.errorMessage}
+                        touched={formElement.config.touched}
+                        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                ))}
+                <div className='row createBookButton'>
+                    
+                </div>
+            </form>
+           
+        );
+        if ( this.state.loading ) {
+            form = <Spinner />;
+        }
+        return (
+            <div className='container'>
+                <h4>Create new book</h4>
+                {form}
+                <button className='btn btn-success' disabled={!this.state.formIsValid}>SAVE</button>
+            </div>
+        );
+    }
 }
 
 export default CreateBook;
